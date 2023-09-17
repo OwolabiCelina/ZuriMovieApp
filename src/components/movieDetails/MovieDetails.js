6d4e2fdba942870141e76ec97d2c61972661fe8e
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2023-09-17 03:38:17 +0100

    runtime in Utc adjusted

--- src/components/movieDetails/MovieDetails.js
+++ src/components/movieDetails/MovieDetails.js
@@ -10,33 +10,43 @@ import './movie-details.css';
 const MovieDetails = () => {
 
     const [back, setBack]= useState("");
-    const [utcTime, setUtcTime] = useState(new Date().toLocaleString('en-US', { timeZone: 'UTC' }));
     
 
     const navigate = useNavigate();
-    const [utcReleaseDate, setUtcReleaseDate] = useState('');
     const location = useLocation()
     const movieData = location.state.data[0]
+    // const [movieDetails, setMovieDetails] = useState(null);
+    // const [error, setError] = useState(null);
+
+    // const movieId = 'https://api.themoviedb.org/3/find/{external_id}'
+    // const apiKey = "6cfc43ab713b0292b2e2b6610ad40c0e"; 
+
+    // useEffect(() =>{
+    //   fetch(`https://api.themoviedb.org/3/movie/${movieId}?${apiKey}`)
+    //   .then((res) =>{
+    //     if(!res.ok){
+    //       throw new Error("Failed to fetch movie details");
+    //     }
+    //     return res.json();
+    //   })
+    //   .then((data) =>{
+    //     const releaseDate = new Date(data.release_date);
+    //     const utcDate = releaseDate.toUTCString();
+    //     setMovieDetails({
+    //       ...data,
+    //       release_date: utcDate
+    //     });
+    //   })
+    //   .catch((err) =>{
+    //     setError(err.message);
+    //   })
+    // }, [movieId])
 
     const handleClick =()=> {
         setBack(back)
         navigate(-1)
     }
 
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setUtcTime(new Date().toLocaleString('en-US', { timeZone: 'UTC' }));
-    }, 1000);
-
-    return () => clearInterval (intervalId);
-  }, []);
-
-  // useEffect(() => {
-  //   const releaseDate = new Date(movieData.release_date);
-  //   const utcReleaseDateString = releaseDate.toISOString();
-  //   setUtcReleaseDate(utcReleaseDateString);
-  // }, [movieData.release_date]);
 
 
 
@@ -59,7 +69,7 @@ const MovieDetails = () => {
             <p data-testid ="movie-title">Title: <span>{movieData.title}</span></p>
             <p data-testid ="movie-release-date">Release Date: <span>{movieData.release_date}</span></p>
             <p data-testid="movie-runtime">Overview: <span>{movieData.overview}</span></p>
-            <p data-testid="movie-overview">Runtime: <span>{utcTime}</span></p>
+            <p data-testid="movie-overview">Runtime: <span>{movieData.runtime}</span></p>
         </div>
 
         <button className='add-btn' onClick={handleClick}>Go Back</button>
